05c9381028b1bc0127d3c9f6dd9b3904034c98d8
Damon Kelley <damon.kelley@gmail.com>
2015-03-06 17:41:04 -0600

    Settings file for executing tests in a sqlite database.

---
+++ tests/settings/test-sqlite.py
@@ -0,0 +1,14 @@
+# Test configuration for quick execution.
+#
+# This settings file will not work for tests against
+# Django 1.6, as it does not support Auto incrementing primary
+# keys in way required by django-name.
+from .base import *
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'TEST_NAME': ':memory:',
+    }
+}
