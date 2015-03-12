1bd855afe6d260c3d3f080a31184ee3ba35eab23
Damon Kelley <damon.kelley@gmail.com>
2015-03-12 16:05:10 -0500

    Add F403 to the ignore list. This is the id for the import * which we intentionally use in the settings files.

--- runtests.py
+++ runtests.py
@@ -12,7 +12,7 @@ PYTEST_ARGS = {
     'fast': ['tests', '-q'],
 }
 
-FLAKE8_ARGS = ['name', 'tests', '--ignore=E501']
+FLAKE8_ARGS = ['name', 'tests', '--ignore=F403,E501']
 
 
 sys.path.append(os.path.dirname(__file__))
