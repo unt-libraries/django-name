b63f932718385c4a8797e69f41bc73071945502e
Damon Kelley <damon.kelley@gmail.com>
2015-03-24 18:52:19 -0500

    More tests for views.

--- tests/test_views.py
+++ tests/test_views.py
@@ -21,17 +21,17 @@ def test_entry_detail_returns_ok(client, name_fixture):
 
 @pytest.mark.xfail
 def test_merged_entry_detail_returns_not_found(client):
-    pass
+    assert False
 
 
 @pytest.mark.xfail
 def test_merged_entry_detail_returns_gone(client):
-    pass
+    assert False
 
 
 @pytest.mark.xfail
 def test_merged_entry_detail_returns_ok(client):
-    pass
+    assert False
 
 
 @pytest.mark.django_db
@@ -55,41 +55,88 @@ def test_label_returns_not_found(client):
     assert 404 == response.status_code
 
 
-@pytest.mark.xfail
-def test_export(client):
-    pass
+@pytest.mark.django_db
+def test_export(client, name_fixture):
+    response = client.get(reverse('name_export'))
+    assert 200 == response.status_code
 
 
-@pytest.mark.xfail
 def test_opensearch(client):
-    pass
+    response = client.get(reverse('name_opensearch'))
+    assert 200 == response.status_code
 
 
-@pytest.mark.xfail
 def test_about(client):
-    pass
+    response = client.get(reverse('name_about'))
+    assert 200 == response.status_code
+
+
+@pytest.mark.django_db
+def test_stats_returns_ok(client, name_fixture):
+    response = client.get(reverse('name_stats'))
+    assert 200 == response.status_code
 
 
 @pytest.mark.xfail
-def test_stats(client):
-    pass
+@pytest.mark.django_db
+def test_stats_returns_ok_with_no_names(client):
+    response = client.get(reverse('name_stats'))
+    assert 200 == response.status_code
 
 
+@pytest.mark.django_db
+def test_get_names_returns_ok(client):
+    response = client.get(reverse('name_names'))
+    assert 200 == response.status_code
+
+
+# TODO: Look at ways that we can confirm that the
+# requests was received as an Ajax call
+@pytest.mark.django_db
+def test_get_names_xhr_returns_ok(client):
+    response = client.get(
+        reverse('name_names'),
+        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    assert 200 == response.status_code
+
+
+# TODO: Take another look at this. We might not need
+#       Allow-Headers on every request, perhaps only ajax
+# TODO: Find a way to test that the origin header is set to '*'
 @pytest.mark.xfail
-def test_get_names(client):
-    pass
+@pytest.mark.django_db
+def test_get_names_has_cors_headers(client):
+    response = client.get(reverse('name_names'))
+    assert response.has_header('Access-Control-Allow-Origin')
+    assert response.has_header('Access-Control-Allow-Headers')
 
 
-@pytest.mark.xfail
+@pytest.mark.django_db
 def test_landing(client):
-    pass
-
-
-@pytest.mark.xfail
-def test_search(client):
-    pass
+    response = client.get(reverse('name_landing'))
+    assert 200 == response.status_code
 
 
 @pytest.mark.xfail
 def test_name_json(client):
-    pass
+    assert False
+
+
+# TODO: Use multiple name fixtures because an empty search
+# query will result in all names being returned
+@pytest.mark.xfail
+@pytest.mark.django_db
+def test_search_with_q(client, name_fixture):
+    url = reverse('name_search') + "?q={}".format(name_fixture.name)
+    response = client.get(url)
+    assert name_fixture.name in response.content
+
+
+@pytest.mark.xfail
+def test_search_with_q_type(client, name_fixture):
+    assert False
+
+
+@pytest.mark.xfail
+def test_search_without_query(client):
+    assert False
