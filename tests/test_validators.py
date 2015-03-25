38539d8c2676ac6ad1bd5b866ae27e46cfd49430
Damon Kelley <damon.kelley@gmail.com>
2015-03-25 17:30:16 -0500

    Placeholders for the validator tests.

---
+++ tests/test_validators.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.mark.xfail
+def test_validate_merged_with():
+    assert False
+
+
+@pytest.mark.xfail
+def test_validate_merged_throws_exception():
+    assert False
+
+
+@pytest.mark.xfail
+def test_validate_merged_cannot_be_merged():
+    assert False
