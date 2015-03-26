4c23948dfd48ceffe2628b2ada890434b9635c5c
Damon Kelley <damon.kelley@gmail.com>
2015-03-26 14:42:58 -0500

    Implements for tests for the views utilities.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,34 +1,67 @@
 import pytest
-from .test_views import twenty_name_fixtures
 from django.test.client import RequestFactory
-from name.views import (
-    normalize_query,
-    resolve_q,
-    resolve_type,
-    filter_names
-)
-
-# FIXME: This is used to silence PEP8 warnings
-twenty_name_fixtures = twenty_name_fixtures
+from name import models, views
 
 
+@pytest.fixture
+def search_names(db, scope='module'):
+    for x in range(1, 5):
+        models.Name.objects.create(
+            name="Personal {}".format(x), name_type=0)
+        models.Name.objects.create(
+            name="Organization {}".format(x), name_type=1)
+        models.Name.objects.create(
+            name="Event {}".format(x), name_type=2)
+        models.Name.objects.create(
+            name="Software {}".format(x), name_type=3)
+        models.Name.objects.create(
+            name="Building {}".format(x), name_type=4)
+    return models.Name.objects.all()
+
+
+# TODO: Find out if this is suppose to return the same id
+#       consecutively.
 @pytest.mark.xfail
 def test_get_unique_user_id():
-    assert False
+    uids = [eval(views.get_unique_user_id()) for i in range(1000)]
+    assert len(uids) == len(set(uids))
 
 
 @pytest.mark.django_db
-def test_filter_names_with_empty_query(twenty_name_fixtures):
-    names = filter_names('', None)
-    assert names.count() == twenty_name_fixtures.count()
+def test_filter_names_with_empty_query(search_names):
+    names = views.filter_names('', None)
+    assert names.count() == search_names.count()
 
 
-# TODO: Create a new fixture where we know how many of
-#       each type is known
 @pytest.mark.django_db
-def test_filter_names_with_query(twenty_name_fixtures):
-    names = filter_names('1', None)
-    assert names.count() == 11
+def test_filter_names_with_query(search_names):
+    names = views.filter_names('1', None)
+    assert names.count() == 5
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('types,expected', [
+    ([0], 1),
+    ([0, 1], 2),
+    ([0, 1, 2], 3),
+    ([0, 1, 2, 3, 4], 5)
+])
+def test_filter_names_with_query_and_name_types(types, expected, search_names):
+    names = views.filter_names('1', types)
+    assert names.count() == expected
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('types,expected', [
+    ([0], 4),
+    ([0, 1], 8),
+    ([0, 1, 2], 12),
+    ([0, 1, 2, 3, 4], 20)
+])
+def test_filter_names_with_no_query_and_name_types(types,
+                                                   expected, search_names):
+    names = views.filter_names('', types)
+    assert names.count() == expected
 
 
 @pytest.mark.parametrize('query,expected', [
@@ -45,19 +78,19 @@ def test_filter_names_with_query(twenty_name_fixtures):
 def test_resolve_type(query, expected):
     rf = RequestFactory()
     request = rf.get('/', {'q_type': query})
-    types = resolve_type(request)
+    types = views.resolve_type(request)
     assert len(types) == expected
 
 
 def test_resolve_q_returns_value():
     rf = RequestFactory()
-    q = resolve_q(rf.get('/', {'q': 'value'}))
+    q = views.resolve_q(rf.get('/', {'q': 'value'}))
     assert q == 'value'
 
 
 def test_resolve_q_returns_empty_string():
     rf = RequestFactory()
-    q = resolve_q(rf.get('/'))
+    q = views.resolve_q(rf.get('/'))
     assert '' == q
 
 
@@ -73,7 +106,7 @@ def test_get_query():
     ('"Pun. cua," tion! !!', 3),
 ])
 def test_normalize_query(query, expected):
-    normalized = normalize_query(query)
+    normalized = views.normalize_query(query)
     assert len(normalized) == expected
 
 
