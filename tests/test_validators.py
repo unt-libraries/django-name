fada9ca40f687f9645fdb203a9f701ef3d17aaa1
Damon Kelley <damon.kelley@gmail.com>
2015-03-26 15:48:51 -0500

    Intitial test for the merged_with validator.

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,16 +1,34 @@
 import pytest
+from name.models import validate_merged_with, Name
+from django.core.exceptions import ValidationError
 
 
-@pytest.mark.xfail
-def test_validate_merged_with():
-    assert False
+@pytest.fixture
+def merge_candidates(db, scope='module'):
+    primary = Name.objects.create(name='Primary', name_type=0)
+    secondary = Name.objects.create(name='Secondary', name_type=0)
+    return (primary, secondary)
 
 
-@pytest.mark.xfail
-def test_validate_merged_throws_exception():
-    assert False
+@pytest.mark.django_db
+def test_validate_merged_with_passes(merge_candidates):
+    primary, secondary = merge_candidates
+    # We will not assert here because failure will raise
+    # an exception and cause a failure. Passing validation
+    # returns nothing and the test passes.
+    validate_merged_with(primary.id)
 
 
-@pytest.mark.xfail
-def test_validate_merged_cannot_be_merged():
-    assert False
+@pytest.mark.django_db
+def test_validate_merged_cannot_be_merged(merge_candidates):
+    '''Tests the validation failure'''
+    primary, secondary = merge_candidates
+
+    secondary.merged_with = primary
+    primary.merged_with = secondary
+
+    secondary.save()
+    primary.save()
+
+    with pytest.raises(ValidationError):
+        validate_merged_with(primary.id)
