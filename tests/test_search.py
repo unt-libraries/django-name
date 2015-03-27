6ce407f80e428ef20bc79f227748d3e0ee969995
Damon Kelley <damon.kelley@gmail.com>
2015-03-27 10:23:13 -0500

    Look up urls using revers. Tests no longer need to be grouped into a class.

--- tests/test_search.py
+++ tests/test_search.py
@@ -4,6 +4,9 @@ import json
 from name.models import Name
 from django.core.urlresolvers import reverse
 
+# All test need access to the database in this file.
+pytestmark = pytest.mark.django
+
 
 @pytest.fixture
 def name_fixtures(db, scope="module"):
@@ -16,30 +19,36 @@ def name_fixtures(db, scope="module"):
     Name.objects.create(name='test building',
                         name_type=4, begin='2000-01-12')
 
+query_template = '?q_type={0}&q={1}'
 
-@pytest.mark.django_db
-class TestNameSearchCase:
 
-    def test_json_returns_expected_results(self, client, name_fixtures):
-        response = client.get('/name/search.json?q_type=Personal&q=person')
+def test_json_returns_expected_results(client, name_fixtures):
+    query = query_template.format('Personal', 'person')
+    response = client.get(reverse('name_names') + query)
 
-        assert json.loads(response.content)[0]['name'] == 'test person'
-        assert response.status_code is 200
+    assert json.loads(response.content)[0]['name'] == 'test person'
+    assert response.status_code is 200
 
-    def test_can_json_search_multiple_name_types(self, client, name_fixtures):
-        response = client.get(
-            '/name/search.json?q_type=Personal,Organization&q=test')
-        json_results = json.loads(response.content)
 
-        assert len(json_results) is 2
-        assert response.status_code is 200
+def test_can_json_search_multiple_name_types(client, name_fixtures):
+    query = query_template.format('Personal,Organization', 'test')
+    response = client.get(
+        reverse('name_names') + query)
+    json_results = json.loads(response.content)
 
-    def test_can_search_multiple_name_types(self, client, name_fixtures):
-        response = client.get(
-            '/name/search/?q_type=Personal,Organization&q=test')
+    assert len(json_results) is 2
+    assert response.status_code is 200
 
-        assert response.status_code is 200
 
-    def test_can_search(self, client, name_fixtures):
-        response = client.get('/name/search/?q_type=Personal&q=test')
-        assert response.status_code is 200
+def test_can_search(client, name_fixtures):
+    query = query_template.format('Personal', 'test')
+    response = client.get(reverse('name_search') + query)
+    assert response.status_code is 200
+
+
+def test_can_search_multiple_name_types(client, name_fixtures):
+    query = query_template.format('Personal,Organization', 'test')
+    response = client.get(
+        reverse('name_search') + query)
+
+    assert response.status_code is 200
