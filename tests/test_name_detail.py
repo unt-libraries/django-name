7245ddb872d8d076b1da31d109408d857d716d8f
Damon Kelley <damon.kelley@gmail.com>
2015-03-27 10:18:44 -0500

    Tests no longer need to be grouped into a class.

--- tests/test_name_detail.py
+++ tests/test_name_detail.py
@@ -1,59 +1,55 @@
+"""
+Check that the correct html properties
+are showing correctly
+"""
 import pytest
 
 from name.models import Name
 from django.core.urlresolvers import reverse
 
 
-@pytest.fixture
-def name_fixtures(db, scope="module"):
-    Name.objects.create(name='person', name_type=0, begin='2012-01-12')
-    Name.objects.create(name='organization', name_type=1, begin='2000-01-12')
-    Name.objects.create(name='event', name_type=2, begin='2500-01-12')
-    Name.objects.create(name='building', name_type=4, begin='2000-01-12')
+pytestmark = pytest.mark.django_db
 
 
-@pytest.mark.django_db
-class TestNameDetailCase:
-    """
-    This class is intended to check that the intended html properties
-    are showing correctly
-    """
-    def test_person_itemprops(self, client):
-        name = Name.objects.create(name='test person',
-                                   name_type=0, begin='2012-01-12')
-        response = client.get(
-            reverse('name_entry_detail', args=[name.name_id]))
+def test_person_itemprops(client):
+    name = Name.objects.create(name='test person',
+                               name_type=0, begin='2012-01-12')
+    response = client.get(
+        reverse('name_entry_detail', args=[name.name_id]))
 
-        assert 'itemprop=\"name\"' in response.content
-        assert 'itemprop=\'url\'' in response.content
-        assert 'itemprop=\"birthDate\"' in response.content
+    assert 'itemprop=\"name\"' in response.content
+    assert 'itemprop=\'url\'' in response.content
+    assert 'itemprop=\"birthDate\"' in response.content
 
-    def test_building_itemprops(self, client):
-        name = Name.objects.create(name='test building',
-                                   name_type=4, begin='2000-01-12')
-        response = client.get(
-            reverse('name_entry_detail', args=[name.name_id]))
 
-        assert 'itemprop=\"name\"' in response.content
-        assert 'itemprop=\'url\'' in response.content
-        assert 'itemprop=\"erectedDate\"' in response.content
+def test_building_itemprops(client):
+    name = Name.objects.create(name='test building',
+                               name_type=4, begin='2000-01-12')
+    response = client.get(
+        reverse('name_entry_detail', args=[name.name_id]))
 
-    def test_organization_itemprops(self, client):
-        name = Name.objects.create(name='test organization',
-                                   name_type=1, begin='2000-01-12')
-        response = client.get(
-            reverse('name_entry_detail', args=[name.name_id]))
+    assert 'itemprop=\"name\"' in response.content
+    assert 'itemprop=\'url\'' in response.content
+    assert 'itemprop=\"erectedDate\"' in response.content
 
-        assert 'itemprop=\"name\"' in response.content
-        assert 'itemprop=\'url\'' in response.content
-        assert 'itemprop=\"foundingDate\"' in response.content
 
-    def test_event_itemprops(self, client):
-        name = Name.objects.create(name='test event',
-                                   name_type=2, begin='2500-01-12')
-        response = client.get(
-            reverse('name_entry_detail', args=[name.name_id]))
+def test_organization_itemprops(client):
+    name = Name.objects.create(name='test organization',
+                               name_type=1, begin='2000-01-12')
+    response = client.get(
+        reverse('name_entry_detail', args=[name.name_id]))
 
-        assert 'itemprop=\"name\"' in response.content
-        assert 'itemprop=\'url\'' in response.content
-        assert 'itemprop=\"startDate\"' in response.content
+    assert 'itemprop=\"name\"' in response.content
+    assert 'itemprop=\'url\'' in response.content
+    assert 'itemprop=\"foundingDate\"' in response.content
+
+
+def test_event_itemprops(client):
+    name = Name.objects.create(name='test event',
+                               name_type=2, begin='2500-01-12')
+    response = client.get(
+        reverse('name_entry_detail', args=[name.name_id]))
+
+    assert 'itemprop=\"name\"' in response.content
+    assert 'itemprop=\'url\'' in response.content
+    assert 'itemprop=\"startDate\"' in response.content
