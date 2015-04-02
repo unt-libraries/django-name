98175b2718fc910230d998441f8ef488f7a38235
Damon Kelley <damon.kelley@gmail.com>
2015-04-02 14:39:09 -0500

    Adds FIXME to mark where a hardcoded URL will need to be modified during javascript refactoring.

--- name/static/name/js/ajax-form.js
+++ name/static/name/js/ajax-form.js
@@ -1,6 +1,7 @@
 $(function() {
     $("#names").autocomplete({
         source: function(request, response){
+            // FIXME: Remove hardcoded URL
             $.getJSON("/name/search.json", {q_type: $('#q_type').val(), q: request.term}, response);
         },
         minLength: 2
