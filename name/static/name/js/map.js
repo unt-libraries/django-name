74bfb39738c298d7d8e5e6a7a9233025aafed089
Damon Kelley <damon.kelley@gmail.com>
2015-04-10 09:28:30 -0500

    Various updates to the map.js. Add default cooridinates in case there are no locations. Add comments.

--- name/static/name/js/map.js
+++ name/static/name/js/map.js
@@ -2,16 +2,21 @@
 
 $(function() {
   var form = $("form[name=map]"),
-      url = form.attr('action'),
-      attribution = form.find('#attribution').html(),
-      tileLayerUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      config = {maxZoom: 18, attribution: attribution};
+    url = form.attr('action'),
+    attribution = form.find('#attribution').html(),
+    tileLayerUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    config = {maxZoom: 18, attribution: attribution};
 
-  var map = L.map('map'),
-      markers = L.markerClusterGroup({showCoverageOnHover: false});
+  // Set a default view incase there are no Locations.
+  var map = L.map('map').setView([0, 0], 2);
 
+  // The markers layer enables us to create marker clusters.
+  var markers = L.markerClusterGroup({showCoverageOnHover: false});
+
+  // Create the Tile Layer and bind it to the map.
   L.tileLayer(tileLayerUrl, config).addTo(map);
 
+  // Get the JSON payload of locations from the endpoint.
   $.get(url).done(function(data) {
     data.forEach(function(v, i){
       // Use the first location to determine where the
@@ -20,15 +25,19 @@ $(function() {
         map.setView([v.fields.latitude, v.fields.longitude], 5);
       }
 
-      // Instantiate a new marker.
       var title = v.fields.belong_to_name.name;
+      // Instantiate a new marker.
       var marker = L.marker(
-        new L.LatLng(v.fields.latitude, v.fields.longitude),
-        { title: title}
+        [v.fields.latitude, v.fields.longitude],
+        {title: title}
       );
 
-      // TODO: Figure out what this does.
-      marker.bindPopup(title);
+      // Leverage jQuery to create an HTML element that we will use
+      // inside the marker. This will create a link to the name detail
+      // page. Then add the new marker to the markers layer, which will 
+      // give us marker clusters.
+      var a = $('<a/>', {'href': v.fields.belong_to_name.url,'text': title });
+      marker.bindPopup(a.prop('outerHTML'));
       markers.addLayer(marker);
     });
 
