2e3c07bb2027122a9be1bd3a8ce097d5e81a7ec2
Damon Kelley <damon.kelley@gmail.com>
2015-05-04 13:34:47 -0500

    Further modification to stats.js.

--- name/static/name/js/stats.js
+++ name/static/name/js/stats.js
@@ -2,85 +2,52 @@
   'use strict';
 })();
 
-var Statistics = function() {
-  this.chartConfig = {
-    chartType: 'LineChart',
-    containerId: 'chart',
-    options: {
-      chartArea: {'height': '80%', 'width': '90%'},
-      hAxis: {
-        slantedText: false,
-      },
-      vAxis: {
-        gridlines: {count: 10},
-        textStyle: {color: 'black', fontName: 'arial', fontSize: 11}
-      },
-      legend: {'position': 'none'}
-    }
-  };
+var DEFAULT_CHART = 0;
 
-  this.control = {
-    controlType: 'ChartRangeFilter',
-    containerId: 'control',
-    options: {
-      filterColumnIndex: 0,
-      ui: {
-        chartType: 'LineChart',
-        chartOptions: {
-          chartArea: {'width': '90%'},
-          hAxis: {'baselineColor': 'none'},
-        },
-        chartView: {
-          columns: [0, 1],
-        },
-        minRangeSize: 86400000,
-      },
-    },
-    state: {
-      range: {start: new Date(2004, 11, 1), end: new Date()},
-    }
-  };
-
+var Statistics = function(options) {
+  this.stage = $(options.target);
   this.dataTables = [];
 
-  this.controlWrapper = new google.visualization.ControlWrapper(this.control);
-  this.chartWrapper = new google.visualization.ChartWrapper(this.chartConfig);
-  this.dashboard = new google.visualization.Dashboard(document.getElementById('dashboard'));
+  this.controlWrapper = new google.visualization.ControlWrapper(options.controlConfig);
+  this.chartWrapper = new google.visualization.ChartWrapper(options.chartConfig);
 
-  this.data = $.ajax({context: this, async: false, url: '/name/stats.json/'}).responseJSON;
-  this.drawDashboard();
-};
-
-Statistics.prototype.drawDashboard = function() {
-  $('.chart-nav').on('click', $.proxy(function(e){
-    var chartId = $(e.target).data('chart-id');
-    this.chartListener(chartId);
-  }, this));
-
-  this.dispatch();
+  this.dashboard = new google.visualization.Dashboard(this.stage);
   this.dashboard.bind(this.controlWrapper, this.chartWrapper);
-  this.chartListener(0);
+
+  this.stage.on('click', '.chart-nav', $.proxy(this.redrawDashboard, this));
+
+  $.ajax({context: this, async: false, url: '/name/stats.json/', success: this.setupDashboard});
 };
 
-Statistics.prototype.chartListener = function(chartId) {
+Statistics.prototype.setupDashboard = function(data) {
+  this.dispatch(data);
+  this.drawDashboard(DEFAULT_CHART);
+};
+
+Statistics.prototype.drawDashboard = function(chartId) {
   this.dashboard.draw(this.dataTables[chartId]);
 };
 
-Statistics.prototype.dispatch = function() {
-  this.createDataTable('created', 'total', 'Date Created');
-  this.createDataTable('created', 'total_to_date', 'Total Created');
-  this.createDataTable('modified', 'total', 'Date Modified');
-  this.createDataTable('modified', 'total_to_date', 'Total Modified');
+Statistics.prototype.redrawDashboard = function(e) {
+  var chartId = $(e.target).data('chart-id');
+  this.drawDashboard(chartId);
 };
 
-Statistics.prototype.createDataTable = function(date_type, total_type, label) {
+Statistics.prototype.dispatch = function(data) {
+  this.createDataTable(data, 'created', 'total', 'Date Created');
+  this.createDataTable(data, 'created', 'total_to_date', 'Total Created');
+  this.createDataTable(data, 'modified', 'total', 'Date Modified');
+  this.createDataTable(data, 'modified', 'total_to_date', 'Total Modified');
+};
+
+Statistics.prototype.createDataTable = function(data, date_type, total_type, label) {
   var dataTable = new google.visualization.DataTable();
   var table = [];
 
   dataTable.addColumn('date', 'Date');
   dataTable.addColumn('number', label);
 
-  $.each(this.data[date_type].stats, function(i, v) {
+  $.each(data[date_type].stats, function(i, v) {
     table.push([new Date(v.month), v[total_type]]);
   });
 
@@ -89,9 +56,39 @@ Statistics.prototype.createDataTable = function(date_type, total_type, label) {
 };
 
 Statistics.create = function() {
-  return new Statistics();
+  var chartConfig = {
+    chartType: 'LineChart',
+    containerId: 'chart',
+    options: {
+      chartArea: {'height': '80%', 'width': '90%'},
+      vAxis: {
+        gridlines: {count: 10},
+      },
+      legend: {'position': 'none'}
+    }
+  };
+
+  var controlConfig = {
+    controlType: 'ChartRangeFilter',
+    containerId: 'control',
+    options: {
+      filterColumnIndex: 0,
+      ui: {
+        chartType: 'LineChart',
+        chartOptions: {
+          chartArea: {'width': '90%', height: '50%'},
+        },
+        minRangeSize: 2678000000, // Milliseconds in 1 month (31 days).
+      },
+    },
+  };
+
+  return new Statistics({
+    controlConfig: controlConfig,
+    chartConfig: chartConfig,
+    target: '#dashboard'
+  });
 };
 
 google.load('visualization', '1.1', {packages: ['corechart', 'controls']});
 google.setOnLoadCallback(Statistics.create);
-
