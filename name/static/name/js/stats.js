76eb351bdf987f0f64fe85bc09fd68c6d6dc0d12
Damon Kelley <damon.kelley@gmail.com>
2015-05-06 10:53:56 -0500

    Renamed some methods. Adds some comments.[ci skip]

--- name/static/name/js/stats.js
+++ name/static/name/js/stats.js
@@ -4,19 +4,29 @@
 
 var DEFAULT_CHART = 0;
 
+/** 
+ * Creates a series of graphs to visually represent
+ * Name data.
+ */
 var Statistics = function(options) {
   this.stage = $(options.stage);
   this.dataTables = [];
 
+  // Configure the Dashboard.
   this.controlWrapper = new google.visualization.ControlWrapper(options.controlConfig);
   this.chartWrapper = new google.visualization.ChartWrapper(options.chartConfig);
 
   this.dashboard = new google.visualization.Dashboard(this.stage[0]);
   this.dashboard.bind(this.controlWrapper, this.chartWrapper);
+
+  // Configure the Pie Chart.
   this.piechart = new google.visualization.PieChart(this.stage.find('#piechart')[0]);
 
+  // Bind the click event on the .chart-nav buttons to trigger the
+  // redrawDashboard method.
   this.stage.on('click', '.chart-nav', $.proxy(this.redrawDashboard, this));
 
+  // Get the Name data.
   $.ajax({
     context: this,
     url: this.stage.find('form').attr('action'),
@@ -24,29 +34,51 @@ var Statistics = function(options) {
   });
 };
 
+/**
+ * Setup the Dashboard
+ *
+ * Creates the DataTables and draws the Dashboard.
+ */
 Statistics.prototype.setupDashboard = function(data) {
-  this.dispatch(data);
+  this.createDataTables(data);
   this.createPieChart(data); 
   this.drawDashboard(DEFAULT_CHART);
 };
 
+/**
+ * Draw the dashboard.
+ * 
+ * chartId is the index of the array this.dataTables that 
+ * is to be displayed.
+ */
 Statistics.prototype.drawDashboard = function(chartId) {
   this.dashboard.draw(this.dataTables[chartId]);
 };
 
+/**
+ * Event handler to redraw the dashboard to display
+ * the designated datatable.
+ */
 Statistics.prototype.redrawDashboard = function(e) {
   var chartId = $(e.target).data('chart-id');
   this.drawDashboard(chartId);
 };
 
-Statistics.prototype.dispatch = function(data) {
-  this.createDataTable(data, 'created', 'total', 'Date Created');
-  this.createDataTable(data, 'created', 'total_to_date', 'Total Created');
-  this.createDataTable(data, 'modified', 'total', 'Date Modified');
-  this.createDataTable(data, 'modified', 'total_to_date', 'Total Modified');
+/** 
+ * Create DataTables for date_created and last_modifed data.
+ */
+Statistics.prototype.createDataTables = function(data) {
+  this.convertToDataTable(data, 'created', 'total', 'Date Created');
+  this.convertToDataTable(data, 'created', 'total_to_date', 'Total Created');
+  this.convertToDataTable(data, 'modified', 'total', 'Date Modified');
+  this.convertToDataTable(data, 'modified', 'total_to_date', 'Total Modified');
 };
 
-Statistics.prototype.createDataTable = function(data, date_type, total_type, label) {
+/**
+ * Convert a JSON object to an array for use
+ * as a DataTable.
+ */
+Statistics.prototype.convertToDataTable = function(data, date_type, total_type, label) {
   var dataTable = new google.visualization.DataTable();
   var table = [];
 
@@ -61,14 +93,18 @@ Statistics.prototype.createDataTable = function(data, date_type, total_type, lab
   this.dataTables.push(dataTable);
 };
 
+/**
+ * Create Pie Chart
+ */
 Statistics.prototype.createPieChart = function(data) {
-  var table = [['type', 'num types']];
-
-  $.each(data.name_type_totals, function(i, v) {
-    table.push([i, v]);
-  });
-
-  var dataTable = google.visualization.arrayToDataTable(table);
+  var dataTable = google.visualization.arrayToDataTable([
+    ['type', 'num types'],
+    ['Personal', data.name_type_totals.personal],
+    ['Organization', data.name_type_totals.organization],
+    ['Building', data.name_type_totals.building],
+    ['Event', data.name_type_totals.event],
+    ['Software', data.name_type_totals.software],
+  ]);
 
   this.piechart.draw(dataTable, {
     title: 'Name Distribution by Type',
@@ -77,6 +113,12 @@ Statistics.prototype.createPieChart = function(data) {
   });
 };
 
+/**
+ * Create
+ *
+ * Class method to create a new Statistics instance. 
+ * Useful as an event handler.
+ */
 Statistics.create = function() {
   var chartConfig = {
     chartType: 'LineChart',
@@ -112,5 +154,6 @@ Statistics.create = function() {
   });
 };
 
+// Get the visualization packages and set the callback.
 google.load('visualization', '1.1', {packages: ['corechart', 'controls']});
 google.setOnLoadCallback(Statistics.create);
