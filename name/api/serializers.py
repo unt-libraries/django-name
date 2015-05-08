36cdc0a393858bd4d67fb0310f3b92d166c9eea8
Damon Kelley <damon.kelley@gmail.com>
2015-05-08 11:29:41 -0500

    Add docstrings to the serializers module.

--- name/api/serializers.py
+++ name/api/serializers.py
@@ -1,8 +1,28 @@
+"""Serializers for the Name App Models.
+
+This module leverages the Django Rest Framework's Serializer
+components to build JSON representations of the models defined
+in this app.
+
+These JSON representations are designed to be backwards compatible
+with the API documented in previous versions.
+
+For documentation regarding the Django Rest Framework Serializers go
+to http://www.django-rest-framework.org/api-guide/serializers/
+"""
+
 from rest_framework import serializers
 from .. import models
 
 
 class IdentifierSerializer(serializers.ModelSerializer):
+    """Serializer for the Identifier Model.
+
+    The following fields have been renamed for backwards compatibility
+    with previous versions of the API.
+        label -> identifier.type
+        href -> identifier.value
+    """
     label = serializers.StringRelatedField(source='type')
     href = serializers.CharField(source='value')
 
@@ -12,6 +32,7 @@ class IdentifierSerializer(serializers.ModelSerializer):
 
 
 class NoteSerializer(serializers.ModelSerializer):
+    """Serializer for the Note Model."""
     type = serializers.SerializerMethodField()
 
     class Meta:
@@ -19,10 +40,16 @@ class NoteSerializer(serializers.ModelSerializer):
         fields = ('note', 'type')
 
     def get_type(self, obj):
+        """Sets the type field.
+
+        Returns the Note Type label, instead of the Note Type ID, which
+        is the default behavior.
+        """
         return obj.get_note_type_label().lower()
 
 
 class VariantSerializer(serializers.ModelSerializer):
+    """Serializer for the Variant Model."""
     type = serializers.SerializerMethodField()
 
     class Meta:
@@ -30,10 +57,30 @@ class VariantSerializer(serializers.ModelSerializer):
         fields = ('variant', 'type')
 
     def get_type(self, obj):
+        """Sets the type field.
+
+        Returns the Variant Type label, instead of the Variant Type ID,
+        which is the default behavior.
+        """
         return obj.get_variant_type_label().lower()
 
 
 class NameSerializer(serializers.ModelSerializer):
+    """Serializer for the Name Model.
+
+    This serializes the the Name model to include detailed information
+    about the object, including the related Variants, Notes, and
+    Identifiers.
+
+    The following fields have been renamed for backwards compatibility
+    with previous versions of the API.
+        authoritative_name -> name.name
+        begin_date -> name.begin
+        end_date -> name.end
+
+    The identifier field is the absolute url to the name_entry_detail
+    page for the model instance.
+    """
     authoritative_name = serializers.CharField(source='name')
     begin_date = serializers.CharField(source='begin')
     name_type = serializers.SerializerMethodField()
@@ -50,10 +97,27 @@ class NameSerializer(serializers.ModelSerializer):
                   'identifier', 'links', 'notes', 'variants',)
 
     def get_name_type(self, obj):
+        """Sets the name_type field.
+
+        Returns the Name Type label, instead of the Name Type ID, which
+        is the default behavior.
+        """
         return obj.get_name_type_label().lower()
 
 
 class NameSearchSerializer(serializers.ModelSerializer):
+    """Name Model Serializer for the Name search/autocompletion
+    endpoint.
+
+    The following fields have been renamed for backwards compatibility
+    with previous versions of the API.
+        begin_date -> name.begin
+        type -> name.get_name_type_label()
+        label -> Formats name.name and name.disambiguation.
+
+    The URL field is the absolute url to the name_entry_detail page for
+    the model instance.
+    """
     begin_date = serializers.CharField(source='begin')
     type = serializers.SerializerMethodField()
     label = serializers.SerializerMethodField()
@@ -66,15 +130,31 @@ class NameSearchSerializer(serializers.ModelSerializer):
                   'disambiguation', 'URL')
 
     def get_type(self, obj):
+        """Sets the type field.
+
+        Returns the Name Type label, instead of the Name Type ID, which
+        is the default behavior.
+        """
         return obj.get_name_type_label().lower()
 
     def get_label(self, obj):
+        """Sets the label field.
+
+        Returns a string in the form of
+            "<name.name> (<name.disambiguation>)"
+        """
         if obj.disambiguation:
             return u'{0} ({1})'.format(obj.name, obj.disambiguation)
         return obj.name
 
 
 class LocationSerializer(serializers.ModelSerializer):
+    """Serailizer for the Locations Model.
+
+    This includes the related Name via the belong_to_name field. The
+    belong_to_name field uses the NameSerializer to nest the related
+    Name model.
+    """
     belong_to_name = NameSerializer()
 
     class Meta:
@@ -82,17 +162,29 @@ class LocationSerializer(serializers.ModelSerializer):
 
 
 class NameStatisticsMonthSerializer(serializers.Serializer):
+    """Serializer for the NameStatisticsMonth object."""
     total = serializers.IntegerField()
     total_to_date = serializers.IntegerField()
     month = serializers.DateTimeField()
 
 
 class NameStatisticsTypeSerializer(serializers.Serializer):
+    """Serializer for the NameStatisticsType object.
+
+    This serializer utilizes the NameStatisticsTypeMonth to serialize
+    the NameStatisticsMonth instances that the object instance contains.
+    """
     running_total = serializers.IntegerField()
     stats = NameStatisticsMonthSerializer(many=True)
 
 
 class NameStatisticsSerializer(serializers.Serializer):
+    """Serializer for the NameStatistics object.
+
+    This serializer utilizes the NameStatisticsTypeSerializer to
+    serialize the NameStatisticsType instances that the object instance
+    contains.
+    """
     created = NameStatisticsTypeSerializer()
     modified = NameStatisticsTypeSerializer()
     name_type_totals = serializers.DictField()
