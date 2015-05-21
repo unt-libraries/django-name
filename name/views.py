730ddc67e6a134735cc6c76b23d244e808d800af
Damon Kelley <damon.kelley@gmail.com>
2015-05-21 15:48:01 -0500

    Make the response message in label match other response messages.

--- name/views.py
+++ name/views.py
@@ -31,12 +31,12 @@ def label(request, name_value):
 
     except Name.DoesNotExist:
         return http.HttpResponseNotFound(
-            u'No matching term found - authoritative, or variant - for \"{0}\"'
+            'No matching term found - authoritative, or variant - for \"{0}\"'
             .format(name_value))
 
     except Name.MultipleObjectsReturned:
         return http.HttpResponseNotFound(
-            u'There are multiple Name objects with the same name: \'{0}\'.'
+            'There are multiple Name objects with the same name: \'{0}\'.'
             .format(normalized_name))
 
 
