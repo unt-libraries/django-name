9d357373a984bef71487ced4628a1fd4befad4a7
Damon Kelley <damon.kelley@gmail.com>
2015-05-22 11:28:05 -0500

    Convert double quotes to single to conform to org style guide.

--- name/models.py
+++ name/models.py
@@ -12,21 +12,21 @@ from .validators import validate_merged_with
 class Identifier_Type(models.Model):
     label = models.CharField(
         max_length=255,
-        help_text="What kind of data is this? Personal website? Twitter?",
+        help_text='What kind of data is this? Personal website? Twitter?',
     )
     icon_path = models.CharField(
         max_length=255,
         blank=True,
-        help_text="Path to icon image?",
+        help_text='Path to icon image?',
     )
     homepage = models.URLField(
         blank=True,
-        help_text="Homepage of label. Twitter.com, Facebook.com, etc",
+        help_text='Homepage of label. Twitter.com, Facebook.com, etc',
     )
 
     class Meta:
-        ordering = ["label"]
-        verbose_name = "Identifier Type"
+        ordering = ['label']
+        verbose_name = 'Identifier Type'
 
     def __unicode__(self):
         return self.label
@@ -34,16 +34,16 @@ class Identifier_Type(models.Model):
 
 class Identifier(models.Model):
     type = models.ForeignKey(
-        "Identifier_Type",
+        'Identifier_Type',
         help_text="Catagorize this record's identifiers here",
     )
-    belong_to_name = models.ForeignKey("Name")
+    belong_to_name = models.ForeignKey('Name')
     value = models.CharField(max_length=500)
     visible = models.BooleanField(default=True)
     order = models.IntegerField(default=0)
 
     class Meta:
-        ordering = ["order", "type"]
+        ordering = ['order', 'type']
 
     def __unicode__(self):
         return self.value
@@ -71,9 +71,9 @@ class Note(models.Model):
         (NOTE_TYPE_OTHER, 'Other'),
     )
 
-    note = models.TextField(help_text="Enter notes about this record here")
+    note = models.TextField(help_text='Enter notes about this record here')
     note_type = models.IntegerField(choices=NOTE_TYPE_CHOICES)
-    belong_to_name = models.ForeignKey("Name")
+    belong_to_name = models.ForeignKey('Name')
 
     objects = NoteManager()
 
@@ -102,19 +102,19 @@ class Variant(models.Model):
         (EXPANSION, 'Expansion'),
         (VARIANT_TYPE_OTHER, 'Other'),
     )
-    belong_to_name = models.ForeignKey("Name")
+    belong_to_name = models.ForeignKey('Name')
     variant_type = models.IntegerField(
         max_length=50,
         choices=VARIANT_TYPE_CHOICES,
-        help_text="Choose variant type.",
+        help_text='Choose variant type.',
     )
     variant = models.CharField(
         max_length=255,
-        help_text="Fill in the other name variants, if any.",
+        help_text='Fill in the other name variants, if any.',
     )
     normalized_variant = models.CharField(
         max_length=255,
-        help_text="NACO normalized variant text",
+        help_text='NACO normalized variant text',
         editable=False,
     )
 
@@ -300,12 +300,12 @@ class Name(models.Model):
     # only one name per record
     name = models.CharField(
         max_length=255,
-        help_text="Please use the general reverse order: LAST, FIRST",
+        help_text='Please use the general reverse order: LAST, FIRST',
     )
 
     normalized_name = models.CharField(
         max_length=255,
-        help_text="NACO normalized form of the name",
+        help_text='NACO normalized form of the name',
         editable=False,
     )
 
@@ -319,27 +319,27 @@ class Name(models.Model):
     begin = models.CharField(
         max_length=25,
         blank=True,
-        help_text="Conforms to EDTF format YYYY-MM-DD",
+        help_text='Conforms to EDTF format YYYY-MM-DD',
     )
 
     # date, month of year of death or un-incorporation of the name
     end = models.CharField(
         max_length=25,
         blank=True,
-        help_text="Conforms to EDTF format YYYY-MM-DD",
+        help_text='Conforms to EDTF format YYYY-MM-DD',
     )
 
     # MusicBrainz derived
     disambiguation = models.CharField(
         max_length=255,
         blank=True,
-        help_text="Clarify to whom or what this record pertains."
+        help_text='Clarify to whom or what this record pertains.'
     )
 
     # bio text - formatted with _markdown_ markup
     biography = models.TextField(
         blank=True,
-        help_text="Compatible with MARKDOWN",
+        help_text='Compatible with MARKDOWN',
     )
 
     # record status flag (active / merged / etc)
@@ -350,7 +350,7 @@ class Name(models.Model):
 
     # if marked merged, value is record id of target to merge with
     merged_with = models.ForeignKey(
-        "self",
+        'self',
         blank=True,
         null=True,
         related_name='merged_with_name',
@@ -485,7 +485,7 @@ class Name(models.Model):
         return self.name_id
 
     class Meta:
-        ordering = ["name"]
+        ordering = ['name']
         unique_together = (('name', 'name_id'),)
 
 
@@ -508,10 +508,10 @@ class Location(models.Model):
     FORMER = 1
 
     LOCATION_STATUS_CHOICES = (
-        (CURRENT, "current"),
-        (FORMER, "former"),
+        (CURRENT, 'current'),
+        (FORMER, 'former'),
     )
-    belong_to_name = models.ForeignKey("Name")
+    belong_to_name = models.ForeignKey('Name')
     latitude = models.DecimalField(
         max_digits=13,
         decimal_places=10,
@@ -544,10 +544,10 @@ class Location(models.Model):
     objects = LocationManager()
 
     class Meta:
-        ordering = ["status"]
+        ordering = ['status']
 
     def geo_point(self):
-        return "%s %s" % (self.latitude, self.longitude)
+        return '{lat} {lng}'.format(lat=self.latitude, lng=self.longitude)
 
     def is_current(self):
         """True if the Location has a status of Current."""
