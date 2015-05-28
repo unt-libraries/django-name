701dea3549a215548e494484143220a463cd07e6
Damon Kelley <damon.kelley@gmail.com>
2015-05-28 14:36:52 -0500

    Update setup.py in anticipation of the release of 1.0.0.

--- setup.py
+++ setup.py
@@ -1,38 +1,45 @@
-
 #! /usr/bin/env python
 import os
+import re
 from setuptools import setup, find_packages
 
-# with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-#     README = readme.read()
+version = ''
+with open('name/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
 
+readme = ''
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as fd:
+    readme = fd.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+install_requires = [
+    'python-dateutil',
+    'markdown2',
+    'djangorestframework >= 3.1',
+    'pynaco == 0.1.0'
+]
 
 setup(
-    name="django-name",
-    version="pre-release",
+    name='django-name',
+    version=version,
     packages=find_packages(),
     include_package_data=True,
-
-    license="BSD",
-    description="Name Authority App for Django.",
-    # long_description=README,
-    keywords="django name citation",
-    author="University of North Texas Libraries",
-    # cmdclass={'test': PyTest},
-    url="https://github.com/unt-libraries/django-name",
+    license='BSD',
+    description='Name Authority App for Django.',
+    long_description=readme,
+    keywords='django name citation',
+    author='University of North Texas Libraries',
+    author_email='mark.phillips@unt.edu',
+    url='https://github.com/unt-libraries/django-name',
+    zip_safe=False,
+    install_requires=install_requires,
     classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application :: User Management"
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application :: User Management'
     ]
 )
