09e962eba3ece1e2a0a3412037981d1bdfbf966c
Damon Kelley <damon.kelley@gmail.com>
2015-06-01 14:12:32 -0500

    Alphabetized the urls for more sane ordering.

--- name/urls.py
+++ name/urls.py
@@ -8,18 +8,18 @@ from .api import views as api
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'stats.json/$', api.stats_json, name='stats-json'),
-    url(r'stats/$', views.stats, name='stats'),
-    url(r'label/(?P<name_value>.*)$', views.label, name='label'),
-    url(r'feed/$', feeds.NameAtomFeed(), name='feed'),
-    url(r'map/$', views.map, name='map'),
-    url(r'locations.json/$', api.locations_json, name='locations-json'),
     url(r'^$', views.landing, name='landing'),
+    url(r'about/$', views.about, name='about'),
     url(r'export/$', views.export, name='export'),
+    url(r'feed/$', feeds.NameAtomFeed(), name='feed'),
+    url(r'label/(?P<name_value>.*)$', views.label, name='label'),
+    url(r'locations.json/$', api.locations_json, name='locations-json'),
+    url(r'map/$', views.locations, name='map'),
+    url(r'opensearch.xml$', views.opensearch, name='opensearch'),
     url(r'search/$', views.SearchView.as_view(), name='search'),
     url(r'search.json$', api.search_json, name="search-json"),
-    url(r'about/$', views.about, name='about'),
-    url(r'opensearch.xml$', views.opensearch, name='opensearch'),
+    url(r'stats.json/$', api.stats_json, name='stats-json'),
+    url(r'stats/$', views.stats, name='stats'),
     url(r'(?P<name_id>.*).json$', api.name_json, name='detail-json'),
     url(r'(?P<name_id>.*).mads.xml$', views.mads_serialize, name='mads-serialize'),
     url(r'(?P<name_id>[^/]+)/', views.detail, name='detail')
