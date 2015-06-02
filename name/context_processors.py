da645038de68e47d14562bfc3543da0ba1066967
Damon Kelley <damon.kelley@gmail.com>
2015-06-02 12:36:52 -0500

    New branding context_processor to add brand settings to the request.

--- name/context_processors.py
+++ name/context_processors.py
@@ -1,5 +1,13 @@
-from name.models import Name
+from .models import Name
+from . import app_settings
 
 
 def name_types(request):
     return {'name_types': dict(Name.NAME_TYPE_CHOICES)}
+
+
+def branding(request):
+    return {
+        'name_app_title': app_settings.NAME_APP_TITLE,
+        'name_admin_email': app_settings.NAME_ADMIN_EMAIL
+    }
