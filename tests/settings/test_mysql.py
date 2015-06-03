67136e516fce90acb8ba8c37f5e1bf7443d83916
Damon Kelley <damon.kelley@gmail.com>
2015-06-03 14:05:33 -0500

    Change the USER env variables so they are passed in the tox env.

--- tests/settings/test_mysql.py
+++ tests/settings/test_mysql.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'name',
-        'USER': os.getenv('MYSQL_USER', default="root"),
+        'USER': os.getenv('DB_MYSQL_USER', default="root"),
         'PASSWORD': os.getenv('DB_PASSWORD', default="root"),
         'HOST': os.getenv('DB_HOST', default='mysql'),
     }
--- tests/settings/test_postgres.py
+++ tests/settings/test_postgres.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'name',
-        'USER': os.getenv('POSTGRES_USER', default='name'),
+        'USER': os.getenv('DB_POSTGRES_USER', default='name'),
         'PASSWORD': os.getenv('DB_PASSWORD', default='name'),
         'HOST': os.getenv('DB_HOST', default='postgres'),
     }
